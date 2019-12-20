c7a95fe1cb1d2a3ac3e41180ede6b67610f026b4
Kah919 <kahyap13@gmail.com>
2019-12-20 02:18:46 -0500

    include proxy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Kah:mongoDbmon13@mernshopping-ktml6.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: `mongodb+srv://kah:${process.env.DB_PW}@namerater-424qa.mongodb.net/test?retryWrites=true&w=majority`
 }
