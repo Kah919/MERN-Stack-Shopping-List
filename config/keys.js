05e64d1e6ab5ddb2035eebc2260599b0e6925b76
Kah919 <kahyap13@gmail.com>
2019-12-20 03:21:38 -0500

    fix authentication issue

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: `mongodb+srv://kah:${process.env.DB_PW}@namerater-424qa.mongodb.net/test?retryWrites=true&w=majority`
+    mongoURI: `mongodb+srv://KahShoppingList:shopping123@mernshopping-ktml6.mongodb.net/test?retryWrites=true&w=majority`
 }
