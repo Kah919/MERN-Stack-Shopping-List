6ed05bfbea024dfc00d0820d94c69eac67f429d3
Kah919 <kahyap13@gmail.com>
2019-12-27 14:15:41 -0500

    create navbar

--- client/front-end/src/App.js
+++ client/front-end/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavBar from './components/AppNavBar';
 import './App.css';
 
 function App() {
   return (
     <>
       <h1>MERN SHOPPING LIST</h1>
+      <AppNavBar />
     </>
   );
 }
