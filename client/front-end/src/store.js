3cd24ce1be631495483300dc26e265803f9930ba
Kah919 <kahyap13@gmail.com>
2019-12-30 03:35:14 -0500

    include notes

--- client/front-end/src/store.js
+++ client/front-end/src/store.js
@@ -15,3 +15,9 @@ const store = createStore(rootReducer, initialState, compose(
 
 export default store;
 
+// createStore - Creates a Redux store that holds the complete state tree of your app. There should only be a single store in your app
+
+// applyMiddleware - Middleware is the suggested way to extend Redux with custom functionality. Middleware lets you wrap the store's dispatch method
+
+// redux-thunk lets the action creators invert control by dispatching functions. They would receive dispatch as an argument and may call it asynchronously
+
